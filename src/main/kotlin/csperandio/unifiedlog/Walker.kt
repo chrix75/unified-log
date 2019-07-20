bbb8148bdd57928eb7bd5349c712a3fa8e4f8063
Christian Sperandio <christian.sperandio@gmail.com>
2019-07-20 11:06:24 +0200

    Add documention generation

--- src/main/kotlin/csperandio/unifiedlog/Walker.kt
+++ src/main/kotlin/csperandio/unifiedlog/Walker.kt
@@ -4,9 +4,17 @@ import csperandio.unifiedlog.events.Event
 import csperandio.unifiedlog.storage.EventStorage
 import java.lang.IndexOutOfBoundsException
 
+/**
+ * Walks along an event storage.
+ */
 class Walker(private val storage: EventStorage) {
     private var offset = 0
 
+    /**
+     * Returns the next event in the storage.
+     *
+     * @return The next Event or null if no event exists.
+     */
     fun next(): Event? {
         return try {
             val e = storage[offset]
